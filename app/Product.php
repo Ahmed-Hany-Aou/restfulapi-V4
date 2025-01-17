1b91e4209296bb44cb31afbe240e79f6797cbf4e
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2025-01-17 06:17:47 +0200

    completed relationships betwee nmodels and fixed stringlength to 191

--- app/Product.php
+++ app/Product.php
@@ -2,12 +2,16 @@
 
 namespace App;
 
+use App\Seller;
+use App\Category;
+use App\Transaction;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    const AVAILABLE_PRODUCT = 'available';
+	const AVAILABLE_PRODUCT = 'available';
 	const UNAVAILABLE_PRODUCT = 'unavailable';
+
     protected $fillable = [
     	'name',
     	'description',
@@ -16,8 +20,24 @@ class Product extends Model
     	'image',
     	'seller_id',
     ];
+
     public function isAvailable()
     {
     	return $this->status == Product::AVAILABLE_PRODUCT;
+    }
+
+    public function seller()
+    {
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
     }
 }
